856589604010c042478d53c8aa1bc16018498275
Rehan Samaratunga <rehandsam@gmail.com>
2024-08-22 12:45:19 -0400

    working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import FrameComponent from "../components/frame-component";
 import styles from "./index.module.css";
 
 const MaintanencePage: NextPage = () => {
-  return (
-    <div className={styles.maintanencePage}>
-      <img
-        className={styles.vectorIcon}
-        loading="lazy"
-        alt=""
-        src="/vector.svg"
-      />
-      <section className={styles.maintanencePageInner}>
-        <FrameComponent />
-      </section>
-    </div>
-  );
+    return (
+        <>
+            <Head>
+                <title>Telemate AI</title>
+                <meta name="description" content="Description of your website" />
+                <link rel="icon" href="/telemateicon.svg" type="image/svg+xml" />
+            </Head>
+            <div className={styles.maintanencePage}>
+                <img
+                    className={styles.vectorIcon}
+                    loading="lazy"
+                    alt=""
+                    src="/vector.svg"
+                />
+                <section className={styles.maintanencePageInner}>
+                    <FrameComponent />
+                </section>
+            </div>
+        </>
+    );
 };
 
 export default MaintanencePage;
